137ced9bdc51486d567d5b9bc48842acde9a32a3
Maria Liang <maria.liang.xv@gmail.com>
2024-06-14 11:22:32 +0200

    finish

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,15 +10,9 @@ export class UserService {
 
   // private baseUrl = 'http://localhost/new-bim/php/favorites.php';
   private baseUrl = 'https://new-bim.000webhostapp.com/php/favorites.php';
-
-  // private userKey = 'currentUser';
   
   constructor( private http: HttpClient ) { }
   
-  // saveFav(userData: any[]): void {
-  //   localStorage.setItem(this.userKey, JSON.stringify(userData));
-  // }
-
   getFavoriteObjects(userid: string): Observable<ObjectBIM[]> {
     return this.http.get<ObjectBIM[]>(`${this.baseUrl}?userid=${userid}`);
   }
